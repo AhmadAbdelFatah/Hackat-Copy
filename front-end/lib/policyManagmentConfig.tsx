2139f8c45f38e4e2a3254b93faf82d7820e834a8
AhmadAbdelFatah <eldeep.lala@gmail.com>
2025-08-25 09:34:36 +0300

    feat(policy): add hide/unhide functionality for policies

--- front-end/lib/policyManagmentConfig.tsx
+++ front-end/lib/policyManagmentConfig.tsx
@@ -2,7 +2,7 @@
 import PolicyManagerABIJson from './PolicyManagerABI.json';
 import type { Abi } from 'viem';
 
-export const POLICY_MANAGER_ADDRESS = '0x57d263aDD2727e40B83ceC864bbE12e10093dc77' as `0x${string}`;
+export const POLICY_MANAGER_ADDRESS = '0xd4cA48CcE956887d08073425Fe2C2B3c0EEd7E52' as `0x${string}`;
 
 
 export const PolicyManagerABI = PolicyManagerABIJson.abi as readonly unknown[] as Abi;
